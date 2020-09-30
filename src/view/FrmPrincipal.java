86d3ac2484ccf53a8cd4d3a6f439749700a11b52
jossrc <kuro.rc10@gmail.com>
2020-09-30 08:16:21 -0500

    Cambiando la apariencia del FrmPrincipal

--- src/view/FrmPrincipal.java
+++ src/view/FrmPrincipal.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,6 +8,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FrmPrincipal extends JFrame {
 
@@ -16,13 +22,19 @@ public class FrmPrincipal extends JFrame {
 	private JPanel contentPane;
 
 	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					FrmPrincipal frame = new FrmPrincipal();
 					frame.setVisible(true);
-				}
-				catch (Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -32,33 +44,54 @@ public class FrmPrincipal extends JFrame {
 	public FrmPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 575, 383);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnSistema = new JMenu("Sistema");
 		menuBar.add(mnSistema);
-		
-		JMenuItem mntmSalir = new JMenuItem("Salir");
+
+		JMenuItem mntmSalir = new JMenuItem("Salir");		
+		mntmSalir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, InputEvent.ALT_MASK));
 		mnSistema.add(mntmSalir);
-		
+
 		JMenu mnMantenimiento = new JMenu("Mantenimiento");
 		menuBar.add(mnMantenimiento);
-		
+
 		JMenuItem mntmClientes = new JMenuItem("Clientes");
 		mnMantenimiento.add(mntmClientes);
-		
-		JMenuItem mntmProductos = new JMenuItem("Productos");
+
+		JMenuItem mntmProductos = new JMenuItem("Productos");		
 		mnMantenimiento.add(mntmProductos);
-		
+
 		JMenu mnReportes = new JMenu("Reportes");
 		menuBar.add(mnReportes);
-		
-		JMenuItem mntmVentas = new JMenuItem("Ventas");
+
+		JMenuItem mntmVentas = new JMenuItem("Ventas");		
 		mnReportes.add(mntmVentas);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		mntmSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		mntmProductos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		mntmVentas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
 	}
 }
