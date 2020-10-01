2fb8d319324265b20528076413f4a2cd1bee0dfe
jossrc <kuro.rc10@gmail.com>
2020-10-01 10:25:01 -0500

    Realizando el Ejercicio2_2 y aplicando formato

--- src/ejercicios/Ejercicio2_2.java
+++ src/ejercicios/Ejercicio2_2.java
@@ -1,18 +1,20 @@
 package ejercicios;
 
-public class Ejercicio2_1 {
+public class Ejercicio2_2 {
 
 	public static void main(String[] args) {
 		System.out.println(1);
 		try {
 			System.out.println(2);
-			int x = 5 / 0;
+			int x = 5 / 2;
 			System.out.println(x);
 			System.out.println(3);
 		} catch (Exception e) {
 			System.out.println(4);
+		} finally {
+			System.out.println(5);
 		}
-		System.out.println(5);
+		System.out.println(6);
 	}
 
 }
