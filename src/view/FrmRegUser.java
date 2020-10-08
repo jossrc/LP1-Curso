ff20c5b78bf629a7218743eeceb14aca90011772
jossrc <kuro.rc10@gmail.com>
2020-10-08 12:04:47 -0500

    Creando métodos para leer los txtbox

--- src/view/FrmRegUser.java
+++ src/view/FrmRegUser.java
@@ -5,11 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.JPasswordField;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.awt.event.ActionEvent;
 
 public class FrmRegUser extends JDialog {
 
@@ -18,6 +22,9 @@ public class FrmRegUser extends JDialog {
 	private JTextField txtApellido;
 	private JTextField txtUsuario;
 	private JPasswordField txtClave;
+	private JDateChooser txtFecha;
+	private JButton btnRegistrar;
+	private JButton btnLimpiar;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -90,17 +97,114 @@ public class FrmRegUser extends JDialog {
 		txtClave.setBounds(77, 95, 136, 20);
 		panel.add(txtClave);
 		
-		JDateChooser txtFecha = new JDateChooser();
+		txtFecha = new JDateChooser();
 		txtFecha.setBounds(76, 124, 137, 20);
 		panel.add(txtFecha);
 		
-		JButton btnRegistrar = new JButton("Registrar");
+		btnRegistrar = new JButton("Registrar");		
 		btnRegistrar.setBounds(312, 10, 97, 33);
 		panel.add(btnRegistrar);
 		
-		JButton btnLimpiar = new JButton("Limpiar");
+		btnLimpiar = new JButton("Limpiar");
 		btnLimpiar.setBounds(312, 66, 97, 33);
 		panel.add(btnLimpiar);
+		
+		btnRegistrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
 
+	}
+	
+	private String encriptar(String clave) {
+		StringBuilder cifrado = new StringBuilder();
+		cifrado.append(clave);
+		
+		for (int i = 0; i < cifrado.length(); i++) {
+			switch (cifrado.charAt(i)) {
+			case 'a':
+				cifrado.setCharAt(i, 'e');
+				break;
+			case 'e':
+				cifrado.setCharAt(i, 'i');
+				break;
+			case 'i':
+				cifrado.setCharAt(i, 'o');
+				break;
+			case 'o':
+				cifrado.setCharAt(i, 'u');
+				break;
+			case 'u':
+				cifrado.setCharAt(i, 'a');
+				break;
+			}
+		}
+		
+		return cifrado.reverse().toString();
+	}
+	
+	private String leerNombre() {
+		
+		if (txtNombre.getText().trim().length() == 0) {
+			JOptionPane.showMessageDialog(null, "El campo nombre está vacío", "Aviso",2);
+			return null;
+		}
+		
+		if (!txtNombre.getText().trim().matches("[a-zA-záéíóúñÑÁÉÍÓÚ]{2,15}")) {
+			JOptionPane.showMessageDialog(null, "Ingrese un nombre válido", "Aviso",2);
+			return null;
+		}
+		
+		return txtNombre.getText();
+	}
+	
+	private String leerApellido() {
+		if (txtApellido.getText().trim().length() == 0) {
+			JOptionPane.showMessageDialog(null, "El campo apellido está vacío", "Aviso",2);
+			return null;
+		}
+		
+		if (!txtApellido.getText().trim().matches("[a-zA-záéíóúñÑÁÉÍÓÚ]{2,15}")) {
+			JOptionPane.showMessageDialog(null, "Ingrese un apellido válido", "Aviso",2);
+			return null;
+		}
+		
+		return txtApellido.getText();
+	}
+
+	private String leerUsuario() {
+		if (txtUsuario.getText().trim().length() == 0) {
+			JOptionPane.showMessageDialog(null, "El campo usuario está vacío", "Aviso",2);
+			return null;
+		}
+		
+		if (!txtUsuario.getText().trim().matches("[a-zA-z0-9]{2,4}")) {
+			JOptionPane.showMessageDialog(null, "Ingrese un usuario válido", "Aviso",2);
+			return null;
+		}
+		
+		return txtUsuario.getText();
+	}
+	
+	private String leerClave() {
+		
+		if (String.valueOf(txtClave.getPassword()).length() == 0) {
+			JOptionPane.showMessageDialog(null, "El campo clave está vacío", "Aviso",2);
+			return null;
+		}
+		
+		if (String.valueOf(txtClave.getPassword()).length() > 5) {
+			JOptionPane.showMessageDialog(null, "Ingrese una clave válida", "Aviso",2);
+			return null;
+		}
+		
+		return encriptar(String.valueOf(txtClave.getPassword()));
+	}
+	
+	private String leerFecha() {
+		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); // Para Visualizar
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); // Para BD
+		return sdf.format(txtFecha.getDate());
 	}
 }
