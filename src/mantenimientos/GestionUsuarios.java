c452c495cae27899afe08812960d55dc6bc2606a
jossrc <kuro.rc10@gmail.com>
2020-10-08 11:41:27 -0500

    Creando paquete para los mantenimientos y generando la Clase GestionUsuarios

---
+++ src/mantenimientos/GestionUsuarios.java
@@ -0,0 +1,34 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.UsuarioInterface;
+import model.Usuario;
+
+public class GestionUsuarios implements UsuarioInterface {
+
+	@Override
+	public int registrar(Usuario u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int eliminar(int codigo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int actualizar(Usuario u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Usuario> listado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
