58dd32b0d824b58cf94d4798becfdb6cac297f9e
jossrc <kuro.rc10@gmail.com>
2020-10-08 11:33:51 -0500

    Creando la clase MySQLConexion para la version 8

---
+++ src/util/MySQLConexion8.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConexion8 {
+	public static Connection getConexion() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://localhost:3306/ciberfarma?useSSL=false&useTimezone=true&serverTimezone=UTC";
+			String usr = "root";
+			String psw = "TU CLAVE MYSQL";
+			con = DriverManager.getConnection(url, usr, psw);
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Error >> Driver no Instalado!!");
+		} catch (SQLException ex) {
+			System.out.println("Error >> de conexión con la BD");
+		} catch (Exception ex) {
+			System.out.println("Error >> general : " + ex.getMessage());
+		}
+		return con;
+	}
+}
