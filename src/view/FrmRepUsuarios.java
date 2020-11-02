1d0cd3db5260d8764cf4116aff7859c4a74bf35c
jossrc <kuro.rc10@gmail.com>
2020-11-02 18:20:35 -0500

    Creando el JDialog FrmRepUsuarios

---
+++ src/view/FrmRepUsuarios.java
@@ -0,0 +1,38 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import javax.swing.UIManager;
+
+public class FrmRepUsuarios extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+				
+				try {
+					FrmRepUsuarios dialog = new FrmRepUsuarios();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public FrmRepUsuarios() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
