e2a1b68b901f625b512ac13572cda87651d9d5d2
jossrc <kuro.rc10@gmail.com>
2020-11-02 14:12:36 -0500

    Configurando la clase Tipos para usarlo con la BD

--- src/model/Tipos.java
+++ src/model/Tipos.java
@@ -2,4 +2,28 @@ package model;
 
 public class Tipos {
 
+	private int id;
+	private String descripcion;
+	
+	public Tipos(int id, String descripcion) {		
+		this.id = id;
+		this.descripcion = descripcion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 }
