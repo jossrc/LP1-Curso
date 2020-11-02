f32ee961d62abbf5d1b30fd670a3585d693c1dec
jossrc <kuro.rc10@gmail.com>
2020-11-02 13:22:16 -0500

    Configurando el método listado de GestionUsuarios

--- src/mantenimientos/GestionUsuarios.java
+++ src/mantenimientos/GestionUsuarios.java
@@ -2,6 +2,7 @@ package mantenimientos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -111,8 +112,43 @@ public class GestionUsuarios implements UsuarioInterface {
 
 	@Override
 	public ArrayList<Usuario> listado() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Usuario> lista = new ArrayList<Usuario>();
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "select * from tb_usuarios";
+			pst = con.prepareStatement(sql);
+
+			rs = pst.executeQuery();
+			
+			while (rs.next()) {
+				int codigo = rs.getInt(1);
+				String nombre = rs.getString(2);
+				String apellido = rs.getString(3);
+				String usuario = rs.getString(4);
+				String clave = rs.getString(5);
+				String fnacim = rs.getString(6);
+				int id_tipo = rs.getInt(7);
+				int estado = rs.getInt(8);
+				
+				Usuario u = new Usuario(codigo, nombre, apellido, usuario, clave, fnacim, id_tipo, estado);
+				lista.add(u);
+			}
+		} catch (Exception e) {
+			System.out.println("Error en listado : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+		
+		return lista;
 	}
 
 }
