c3caa29071f54f9dab74bacc8fe409a517b10a9e
jossrc <kuro.rc10@gmail.com>
2020-11-02 14:22:29 -0500

    Implementando el metodo listado en el GestionTipos

--- src/mantenimientos/GestionTipos.java
+++ src/mantenimientos/GestionTipos.java
@@ -16,7 +16,37 @@ public class GestionTipos implements TiposInterface {
 	public ArrayList<Tipos> listado() {
 		
 		ArrayList<Tipos> lista = new ArrayList<Tipos>();
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
 		
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "select * from tb_tipo";
+
+			pst = con.prepareStatement(sql);		
+
+			rs = pst.executeQuery();			
+			
+			while(rs.next()) {	
+				
+				int id = rs.getInt(1);
+				String descripcion = rs.getString(2);
+				
+				Tipos t = new Tipos(id, descripcion);
+				
+				lista.add(t);
+			}
+
+		} catch (Exception e) {
+			System.out.println("Error en listado (Tipos) " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
 		
 		return lista;
 	}
