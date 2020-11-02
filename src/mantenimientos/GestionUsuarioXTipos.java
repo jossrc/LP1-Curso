ba41e1ce4a9ac26020767665ae3fcf755a62709d
jossrc <kuro.rc10@gmail.com>
2020-11-02 17:46:38 -0500

    Creando el manten. GestionUsuarioXTipos

---
+++ src/mantenimientos/GestionUsuarioXTipos.java
@@ -0,0 +1,23 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.UsuarioXTipoInterface;
+import model.RepUsuarioTipo;
+import model.Usuario;
+
+public class GestionUsuarioXTipos implements UsuarioXTipoInterface {
+
+	@Override
+	public ArrayList<Usuario> listadoUsuariosxTipo(int tipo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RepUsuarioTipo> listadoUsuariosYTipo(int tipo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
