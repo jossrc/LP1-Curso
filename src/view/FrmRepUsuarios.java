670b128e4dadfd20b28ad593d1e9d42a92b717fb
jossrc <kuro.rc10@gmail.com>
2020-11-02 18:34:23 -0500

    Aplicando estilos al FrmRepUsuarios

--- src/view/FrmRepUsuarios.java
+++ src/view/FrmRepUsuarios.java
@@ -4,6 +4,16 @@ import java.awt.EventQueue;
 
 import javax.swing.JDialog;
 import javax.swing.UIManager;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Cursor;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 
 public class FrmRepUsuarios extends JDialog {
 
@@ -31,8 +41,41 @@ public class FrmRepUsuarios extends JDialog {
 	}
 
 	public FrmRepUsuarios() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Reporte de Usuarios");
+		setBounds(100, 100, 573, 395);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 557, 363);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("LISTADO DE USUARIOS");
+		lblTitulo.setOpaque(true);
+		lblTitulo.setForeground(Color.WHITE);		
+		lblTitulo.setBackground(new Color(86, 203, 244 ));
+		lblTitulo.setFont(new Font("SansSerif", Font.BOLD, 14));
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setBounds(10, 11, 537, 39);
+		panel.add(lblTitulo);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 69, 537, 228);
+		panel.add(scrollPane);
+		
+		JTextArea txtS = new JTextArea();
+		scrollPane.setViewportView(txtS);
+		
+		JButton btnReporte = new JButton("REPORTE");
+		btnReporte.setForeground(Color.WHITE);
+		btnReporte.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnReporte.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnReporte.setContentAreaFilled(false);
+		btnReporte.setOpaque(true);
+		btnReporte.setBorder(null);
+		btnReporte.setBackground(new Color(162, 164, 165));
+		btnReporte.setBounds(219, 308, 118, 37);
+		panel.add(btnReporte);
 
 	}
-
 }
