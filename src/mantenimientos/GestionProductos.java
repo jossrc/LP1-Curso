479e7ed7384da42a300893a70c3472da1245f8b4
jossrc <kuro.rc10@gmail.com>
2020-11-03 21:25:09 -0500

    Creando el mantenimiento GestionProductos

---
+++ src/mantenimientos/GestionProductos.java
@@ -0,0 +1,16 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.ProductoInterface;
+import model.Producto;
+
+public class GestionProductos implements ProductoInterface {
+
+	@Override
+	public ArrayList<Producto> listadoProductos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
