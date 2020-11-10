9a7824959fcb9c812fe0a13ceb7ae6c19c18e4a8
jossrc <kuro.rc10@gmail.com>
2020-11-10 11:35:22 -0500

    Desarrollando el metodo generaNumBoleta

--- src/mantenimientos/GestionVenta.java
+++ src/mantenimientos/GestionVenta.java
@@ -1,17 +1,49 @@
 package mantenimientos;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import java.util.ArrayList;
 
 import interfaces.VentaInferface;
 import model.CabeceraBoleta;
 import model.DetalleBoleta;
+import util.MySQLConexion8;
 
 public class GestionVenta implements VentaInferface {
 
 	@Override
 	public String generaNumBoleta() {
-		// TODO Auto-generated method stub
-		return null;
+		String codigo = "B0001"; // Default
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				DecimalFormat df = new DecimalFormat("0000");
+				codigo = "B" + df.format(Integer.parseInt(rs.getString(1)) + 1);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Error en generar nm boleta : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+
+		return codigo;
 	}
 
 	@Override
