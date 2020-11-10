64d6dde5ba6dc903e25bd797e2eb51bc706f329b
jossrc <kuro.rc10@gmail.com>
2020-11-10 11:15:05 -0500

    Creando el mantenimiento GestionVenta

---
+++ src/mantenimientos/GestionVenta.java
@@ -0,0 +1,23 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.VentaInferface;
+import model.CabeceraBoleta;
+import model.DetalleBoleta;
+
+public class GestionVenta implements VentaInferface {
+
+	@Override
+	public String generaNumBoleta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int realizarVenta(CabeceraBoleta cabeBol, ArrayList<DetalleBoleta> detBol) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
